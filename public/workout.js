bbdf6587d8859298c4791d3ba963d44e41e3d554
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-12-11 20:41:20 -0500

    restructured object to eliminate redundancy

--- public/workout.js
+++ public/workout.js
@@ -6,18 +6,21 @@ async function initWorkout() {
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
 
-    const lastWorkoutSpecs = lastWorkout.exercises[lastWorkout.exercises.length-1];
+      const lastWorkoutSpecs = lastWorkout.exercises[lastWorkout.exercises.length - 1];
+      console.log(lastWorkoutSpecs)
     // date, name, type, reps, sets, weight  type, duration
     const workoutSummary = {
-      date: formatDate(lastWorkout.day),
-      name: lastWorkoutSpecs.name,
-      type: lastWorkoutSpecs.type,
-      durationToday: lastWorkoutSpecs.duration,
-      // totalDayDuration: lastWorkout.totalDuration,
-      numExercises: lastWorkout.exercises.length,
-      ...tallyExercises(lastWorkout.exercises)
-    };
-    console.log(workoutSummary);
+      sharedStats: {
+        date: formatDate(lastWorkout.day),
+        name: lastWorkoutSpecs.name,
+        type: lastWorkoutSpecs.type,
+        durationToday: lastWorkoutSpecs.duration,
+        numExercises: lastWorkout.exercises.length,
+      },
+      specificStats: {
+        ...tallyExercises(lastWorkoutSpecs)
+      }
+    }
 
     renderWorkoutSummary(workoutSummary);
   } else {
@@ -26,8 +29,9 @@ async function initWorkout() {
 }
 
 function tallyExercises(exercises) {
-  console.log(exercises[0].type);
-  const tallied = exercises.reduce((acc, curr) => {
+  console.log(exercises)
+  const tallied = Object.values(exercises).reduce((acc, curr) => {
+    console.log(curr.type)
     if (curr.type === "resistance") {
       acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
@@ -60,39 +64,60 @@ function renderWorkoutSummary(summary) {
 
   const container = document.querySelector(".workout-stats");
 
-  const workoutKeyMap = summary.type === "cardio" ? {
-    date: "Date",
-    name: "Name",
-    type: "Type",
-    durationToday: "Last Workout Duration",
-    numExercises: "Exercise's Performed",
-    totalDuration: "Today's Total Duration",
-    totalDistance: "Total Distance Covered"
+  const workoutKeyMap = summary.sharedStats.type === "cardio" ? {
+    sharedStats: {
+      date: "Date",
+      name: "Name",
+      type: "Type",
+      durationToday: "Last Workout Duration",
+      numExercises: "Exercise's Performed",
+
+    },
+
+    specificStats: {
+      totalDuration: "Today's Total Duration",
+      totalDistance: "Total Distance Covered"
+    }
   } :
-  {
-    date: "Date",
-    name: "Name",
-    type: "Type",
-    durationToday: "Last Workout Duration",
-    numExercises: "Exercise's Performed",
-    totalDuration: "Today's Total Duration",
-    totalWeight: "Total Weight",
-    totalSets: "Total Sets Performed",
-    totalReps: "Total Reps Performed",
+    {
+      sharedStats: {
+        date: "Date",
+        name: "Name",
+        type: "Type",
+        durationToday: "Last Workout Duration",
+        numExercises: "Exercise's Performed",
+
+      },
+
+      specificStats: {
+        totalDuration: "Today's Total Duration",
+        totalWeight: "Total Weight",
+        totalSets: "Total Sets Performed",
+        totalReps: "Total Reps Performed",
+      }
+    }
+
+    ;
+  console.log(workoutKeyMap)
+
+
+
+  for (const [k, v] of Object.entries(summary)) {
+    console.log(v)
+    for (const [key, value] of Object.entries(v)) {
+      console.log(key, value, v)
+      const p = document.createElement("p");
+      const strong = document.createElement("strong");
+
+      strong.textContent = workoutKeyMap[key];
+      const textNode = document.createTextNode(`: ${summary[key]}`);
+
+      p.appendChild(strong);
+      p.appendChild(textNode);
+
+      container.appendChild(p);
+    }
   };
-
-  Object.keys(workoutKeyMap).forEach(key => {
-    const p = document.createElement("p");
-    const strong = document.createElement("strong");
-
-    strong.textContent = workoutKeyMap[key];
-    const textNode = document.createTextNode(`: ${summary[key]}`);
-
-    p.appendChild(strong);
-    p.appendChild(textNode);
-
-    container.appendChild(p);
-  });
 }
 
 function renderNoWorkoutText() {
