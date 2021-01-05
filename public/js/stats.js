3fb73113c2a148985809f3fb86615316dc5ae51b
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-01-04 23:46:20 -0500

    removed additional unnused code

--- public/js/stats.js
+++ public/js/stats.js
@@ -172,22 +172,6 @@ function populateChart(data) {
     }
   });
 }
-//////////////NOT BEING USED -> MOVED TO UTILFUNCTIONS.JS///////////
-// const formatDate = () => {
-//   //shows only the dates for the current week
-
-//   let d = new Date();
-
-//   let datesArr = new Array();
-
-//   for (let i = 0; i <= 6; i++) {
-//     let date = i - d.getDay();
-//     let day = new Date(d.setDate(d.getDate() + date))
-
-//     datesArr.push(`${d.getMonth() + 1}/${day.getDate()}/${d.getFullYear()}`)
-//   }
-//   return datesArr;
-// }
 
 const duration = (data) => {
 
